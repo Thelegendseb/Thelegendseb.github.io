0328faa668734acbf7d452aa9b25551976c0f7fd
Sebastian Clarke <sebclarke1@outlook.com>
2023-01-26 22:50:17 +0000

    soft blob imported

---
+++ js/softblob.js
@@ -0,0 +1,130 @@
+// center point
+let centerX = 0.0, centerY = 0.0;
+
+let radius = 75, rotAngle = -90;
+let accelX = 0.0, accelY = 0.0;
+let deltaX = 0.0, deltaY = 0.0;
+let springing = 0.0009, damping = 0.98;
+
+let scale = 8;
+
+var id = 'p5-canvas';
+
+//corner nodes
+let nodes = 6;
+
+//zero fill arrays
+let nodeStartX = [];
+let nodeStartY = [];
+let nodeX = [];
+let nodeY = [];
+let angle = [];
+let frequency = [];
+
+// soft-body dynamics
+let organicConstant = 1.0;
+
+function setup() {
+  setup_Sketch();
+
+  radius = height / scale;
+
+  //center shape in window
+  centerX = width / 2;
+  centerY = height / 2;
+
+  //initialize arrays to 0
+  for (let i = 0; i < nodes; i++){
+    nodeStartX[i] = 0;
+    nodeStartY[i] = 0;
+    nodeY[i] = 0;
+    nodeY[i] = 0;
+    angle[i] = 0;
+  }
+
+  // iniitalize frequencies for corner nodes
+  for (let i = 0; i < nodes; i++){
+    frequency[i] = random(5, 12);
+  }
+
+  noStroke();
+  frameRate(60);
+}
+
+function setup_Sketch() {
+  var elem = document.getElementById(id);
+  var properties = window.getComputedStyle(elem, null);
+  var height = properties.height;
+  var width = properties.width;
+  var c = createCanvas(parseInt(width),parseInt(height));
+  c.parent(id);
+}
+
+function windowResized() {
+  var elem = document.getElementById(id);
+  var properties = window.getComputedStyle(elem, null);
+  var height = properties.height;
+  var width = properties.width;
+  resizeCanvas(parseInt(width),parseInt(height));
+
+  radius = height / scale;
+}
+
+function draw() {
+  //fade background
+  fill(12, 12, 12, 100);
+  rect(0, 0, width, height);
+  drawShape();
+  moveShape();
+}
+
+function drawShape() {
+  //  calculate node  starting locations
+  for (let i = 0; i < nodes; i++){
+    nodeStartX[i] = centerX + cos(radians(rotAngle)) * radius;
+    nodeStartY[i] = centerY + sin(radians(rotAngle)) * radius;
+    rotAngle += 360.0 / nodes;
+  }
+
+  // draw polygon
+  curveTightness(organicConstant);
+  fill(46, 255, 116);
+  beginShape();
+  for (let i = 0; i < nodes; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  for (let i = 0; i < nodes-1; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  endShape(CLOSE);
+}
+
+function moveShape() {
+  //move center point
+  deltaX = mouseX - centerX;
+  deltaY = mouseY - centerY;
+
+  // create springing effect
+  deltaX *= springing;
+  deltaY *= springing;
+  accelX += deltaX;
+  accelY += deltaY;
+
+  // move predator's center
+  centerX += accelX;
+  centerY += accelY;
+
+  // slow down springing
+  accelX *= damping;
+  accelY *= damping;
+
+  // change curve tightness
+  organicConstant = 1 - ((abs(accelX) + abs(accelY)) * 0.1);
+
+  //move nodes
+  for (let i = 0; i < nodes; i++){
+    nodeX[i] = nodeStartX[i] + sin(radians(angle[i])) * (accelX * 2);
+    nodeY[i] = nodeStartY[i] + sin(radians(angle[i])) * (accelY * 2);
+    angle[i] += frequency[i];
+  }
+}
